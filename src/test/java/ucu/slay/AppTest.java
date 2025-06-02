5b1c67f010ca219d952ec184dcdbd93476ba9b43
Guzman Zugnoni <guzman.zugnoni@correo.ucu.edu.uy>
2025-06-02 18:29:48 -0300

    init

---
+++ src/test/java/ucu/slay/AppTest.java
@@ -0,0 +1,11 @@
+package ucu.slay;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+	@Test
+	public void nada() {
+	}
+}
